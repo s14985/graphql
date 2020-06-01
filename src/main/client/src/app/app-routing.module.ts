121f83ec4bbecefebffc297cf45c7a1d08c080e3
s14985@pjwstk.edu.pl <C%3Rwien>
2020-06-01 21:44:50 +0200

    01.06.2020 added navbar and footer

--- src/main/client/src/app/app-routing.module.ts
+++ src/main/client/src/app/app-routing.module.ts
@@ -1,13 +1,40 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { OrdersComponent } from './orders/orders.component';
 import { GraphQLModule } from './graphql.module';
+import { OrdersComponent } from './items/orders/orders.component';
+import { HomeComponent } from "./home/home.component";
+import { ItemsComponent } from "./items/items.component";
+import { ProductDetailsComponent } from "./items/product-details/product-details.component";
+import { ProductsListComponent } from "./items/products-list/products-list.component";
 
 const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
 	{
 		path: 'orders',
 		component: OrdersComponent,
 	},
+  {
+    path: 'items',
+    component: ItemsComponent,
+    children: [
+      {
+        path: '',
+        component: ProductsListComponent
+      },
+      {
+        path: ':id',
+        component: ProductDetailsComponent
+      }
+    ]
+  },
 ];
 
 @NgModule({
--- src/main/client/src/app/app.module.ts
+++ src/main/client/src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProductsComponent } from './products/products.component';
-import { OrdersComponent } from './orders/orders.component';
+import { OrdersComponent } from './items/orders/orders.component';
 import { GraphQLModule } from './graphql.module';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { ErrorComponent } from './shared/error/error.component';
+import { NavItemComponent } from './shared/navbar/nav-item/nav-item.component';
+import { ProductDetailsComponent } from './items/product-details/product-details.component';
+import { ItemsComponent } from './items/items.component';
+import { ProductsListComponent } from './items/products-list/products-list.component';
+import { ShoppingCartComponent } from './items/shopping-cart/shopping-cart.component';
 
 @NgModule({
-	declarations: [AppComponent, ProductsComponent, OrdersComponent],
+	declarations: [AppComponent, OrdersComponent, HomeComponent, NavbarComponent, FooterComponent, ErrorComponent, NavItemComponent, ProductDetailsComponent, ItemsComponent, ProductsListComponent, ShoppingCartComponent],
 	imports: [
 		BrowserModule,
 		AppRoutingModule,
